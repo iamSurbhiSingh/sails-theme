b8e5ca208de150957f17b5a46d5c209c886bfdf6
iamSurbhiSingh <iamsurbhi.92@gmail.com>
2016-11-04 16:03:07 +0530

    design added

---
+++ custom/js/custom.js
@@ -0,0 +1,26 @@
+$(function(){
+	// jq-srink
+	// jq-menu-collapse
+	$('.jq-menu-collapse').click(function(e){
+		e.preventDefault();
+		var self = $(this);
+		var status = self.attr('data-status');
+		if(status == 'expanded')
+		{
+			$('.jq-srink').hide();
+			$('.profile-main').hide();
+			$('.menu-title').hide();
+			$('.sidebar-container').css("width","55px");
+			$('.navbar-brand').css("width","55px");
+			self.attr('data-status','collapse');
+		}
+		else {
+			$('.jq-srink').show();
+			$('.profile-main').show();
+			$('.menu-title').show();
+			$('.sidebar-container').css("width","220px");
+			$('.navbar-brand').css("width","220px");
+			self.attr('data-status','expanded');
+		}
+	});
+});
